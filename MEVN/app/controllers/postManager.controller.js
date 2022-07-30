fdd6994fd9f90b5809213e13abe70b925a567163
anjenn <93240896+anjenn@users.noreply.github.com>
2022-07-31 01:06:02 +0200

    adding backend for post & login

---
+++ MEVN/app/controllers/postManager.controller.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+const postManager = db.postManager;
+// Create and Save a new post
+exports.create = (req, res) => {};
+// Retrieve all posts from the database.
+exports.findAll = (req, res) => {};
+// Find a single post with an id
+exports.findOne = (req, res) => {};
+// Update a post by the id in the request
+exports.update = (req, res) => {};
+// Delete a post with the specified id in the request
+exports.delete = (req, res) => {};
+// Delete all post from the database.
+exports.deleteAll = (req, res) => {};
+// Find all published posts
+exports.findAllPublished = (req, res) => {};
