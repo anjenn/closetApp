0324b04be05a5ca586c61a13213d219326e7c795
anjenn <93240896+anjenn@users.noreply.github.com>
2022-08-27 23:28:09 +0200

    api calls implemented

--- MEVN/app/controllers/post.controller.js
+++ MEVN/app/controllers/post.controller.js
@@ -69,10 +69,59 @@ exports.findOne = (req, res) => {
     });
 };
 // Update a post by the id in the request
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+  const id = req.params.id;
+  Post.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Post with id=${id}. Maybe Post was not found!`,
+        });
+      } else res.send({ message: "Post was updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Post with id=" + id,
+      });
+    });
+};
 // Delete a post with the specified id in the request
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Post.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Post with id=${id}. Maybe Post was not found!`,
+        });
+      } else {
+        res.send({
+          message: "Post was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Post with id=" + id,
+      });
+    });
+};
 // Delete all post from the database.
-exports.deleteAll = (req, res) => {};
-// Find all published posts
-exports.findAllPublished = (req, res) => {};
+exports.deleteAll = (req, res) => {
+  Post.deleteMany({})
+    .then((data) => {
+      res.send({
+        message: `${data.deletedCount} Posts were deleted successfully!`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while removing all Posts.",
+      });
+    });
+};
