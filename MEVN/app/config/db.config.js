4390b74c14ec11f47acd7ae0003efa0b14ba2e61
Jenn An <93240896+anjenn@users.noreply.github.com>
2022-10-14 19:37:37 +0200

    github configuration done

--- MEVN/app/config/db.config.js
+++ MEVN/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://anjenn:qlalfqjsghmongo@cluster0.ykj0mk7.mongodb.net/?retryWrites=true&w=majority",
+  url: "mongodb+srv://anjenn:juqnfHdVso3xeS2e@cluster0.dsh8zr5.mongodb.net/?retryWrites=true&w=majority",
 };
