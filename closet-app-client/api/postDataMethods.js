906f0fdfce900af98cadc0c138b6e0115614a811
Jenn An <93240896+anjenn@users.noreply.github.com>
2022-10-21 00:34:01 +0200

    final list of post CRUD methods decided

--- closet-app-client/api/postDataMethods.js
+++ closet-app-client/api/postDataMethods.js
@@ -18,11 +18,19 @@ class postDataMethods {
   getUserData() { // retrieve all posts in myPage
     return http.get(`/MyPage`); // SAVED/LIKED
   }
-  // - not in routes.js
-  findByTag(tag) { // get a post via tag
-    return http.get(`/FeedView?tag=${tag}`);
-  }
-
-  
 }
 export default new postDataMethods();
+
+
+// Q1. How can it tell it is a method about post, from post api?
+// - does it just search anything from api with the same endpoint, and same crud?
+// Q2. Is the function here the same thing as what is defined in post.controller.js?
+// Q3. how do i pass parameter here, when that's not how the functions are written in the api?
+/*
+findByTitle(title) {
+    return http.get(`/tutorials?title=${title}`);
+  }
+
+How can the endpoint of this be written in post.routes.js?
+or is is just sth done in the frontend level, where it filters the collection of data given to the specific endpoint,
+*/
