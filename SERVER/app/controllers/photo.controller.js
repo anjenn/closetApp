262180bc0d7388d037c5d313674480eb32aa8dc2
Jenn An <93240896+anjenn@users.noreply.github.com>
2022-10-24 23:30:22 +0200

    folder names changed

---
+++ SERVER/app/controllers/photo.controller.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const Photo =  require("../models/photo.model.js")(mongoose);
+
+/*
+In the callback, we have a res and a req. res means response, and req means request. We use res for sending responses to our client, like Userman, or any front-end client. And we use req for receiving requests from a client app like Postman, or any front-end client.
+*/
+
+exports.createPhoto = (req, res) => {
+    // Validate request
+    if (!req.body.url) {
+      res.status(400).send({ message: "Content cannot be empty" });
+      return;
+    }
+    // Create a Photo
+    const photo = new Photo({
+        postID: req.body.postID,
+        order: req.body.order,
+        url: req.body.url,
+        edits: req.body.edits,
+    });
+    photo
+      .save(photo)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Photo."
+        });
+    });
+};
+exports.getPhotos = (req, res) => {
+    const postID = req.params.postID;
+    const order = req.params.order;
+    console.log(postID, order);
+
+    Photo.findById(photoID)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Photo with id " + photoID });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Photo with id=" + photoID });
+    });
+};
+exports.deletePhoto = (req, res) => {
+  const photoID = req.params.id;
+  Photo.findByIdAndRemove(photoID)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Photo with id=${photoID}. Maybe Photo was not found!`
+        });
+      } else {
+        res.send({
+          message: "Photo was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Photo with id=" + photoID
+      });
+    });
+}
+exports.updatePhoto = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+  const photoID = req.params.id;
+  Photo.findByIdAndUpdate(userID, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Photo with id=${userID}. Maybe Photo was not found!`
+        });
+      } else res.send({ message: "Photo was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Photo with id=" + userID
+      });
+    });
+}
